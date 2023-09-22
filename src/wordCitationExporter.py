b08a8c68eb1c081e70a71c9c287e0df836131f0c
Teng Zhang <ztnuaa0211@gmail.com>
2023-09-22 11:30:26 +0800

    Improve the utility: wordCitationExporter.
        - Extract all the citation keys from the word document.
        - Find all the citation entries in the Zotero bib lib file and
          output these entries in a seperate bib file.

--- src/wordCitationExporter.py
+++ src/wordCitationExporter.py
@@ -1,8 +1,9 @@
 
-from typing import List
 import zipfile
 import xml.etree.ElementTree as ET
 import json
+import argparse
+import bibtexparser
 
 import sys
 import os
@@ -20,7 +21,7 @@ def docParser(docFile):
     # Under the body tag, find all the paragraph sections
     p_sections = body.findall('w:p', ns) 
 
-    citations = []
+    citeText = []
 
     # Find all the citation items
     for p in p_sections:
@@ -28,33 +29,87 @@ def docParser(docFile):
 
         for item in instrText:
             text = item.text
-            # Check whether the instrText is citation
-            if "citation-key" in text:
-                key = extractCitations(text)
-                print(key)
+            # Put all text to a list
+            citeText.append(text)
 
-    return
+    citeKeys = extractCitations(citeText)
 
-def extractCitations(text):
+    return citeKeys
+
+def extractCitations(citeText):
     '''Extract citation-key from the text string
     '''
 
-    print(text)
+    text = "".join(citeText)
     items =  text.split('"')
+    citeKeys = []
 
     # Find the index of citation-key
-    iCK = items.index('citation-key')
+    #iCK = items.index('citation-key')
+    indicies = [i for i, item in enumerate(items) if item == "citation-key"]
+    
+    for i in indicies:
+        citeKeys.append(items[i+2])
 
-    citeKey = items[iCK+2]
+    # Remove duplicate items
+    citeKeys = set(citeKeys)
 
-    return citeKey
+    return citeKeys
+
+def extractBibfile(citeKeys,bib_file_name):
+    '''Export bib file containing all the citation entries from the zotero library
+    '''
+
+    bib_lib = bibtexparser.parse_file(bib_file_name)
+
+    export_lib = bibtexparser.parse_string("")
+    for entry in bib_lib.entries:
+        for key in citeKeys:
+            if key == entry.key:
+                export_lib.add(entry)
+                break
+
+    bibtexparser.write_file("output.bib",export_lib)
+
+    return
+
+def getArgs():
+    """Built scripts command line arguments
+    """
+    parser = argparse.ArgumentParser(
+                description = "wordCitationExporter: A utility to extract citaion keys from the word document"
+                )
+
+    parser.add_argument(
+            '-f',
+            '--file',
+            type = str,
+            help = " Zotero automatic exported better bibtex lib file.",
+            required = True
+            )
+
+    parser.add_argument(
+            '-d',
+            '--document',
+            type = str,
+            help = " Original word document.",
+            required = True
+            )
+
+    return(parser.parse_args())
 
 def main():
-   
-    #docFile = sys.args()[1]
-    docFile = "LES-spray-flow-EN-v2-manuscript.docx"
+    
+    args = getArgs()
 
-    docParser(docFile)
+    bib_file_name = args.file
+
+    #docFile = "LES-spray-flow-EN-v2-manuscript.docx"
+    doc_file = args.document
+
+    citeKeys = docParser(doc_file)
+
+    extractBibfile(citeKeys,bib_file_name)
 
 if __name__ == "__main__":
     main()   
